69ef5a514a9ca793cd2a0972f0626fd6dae25e0d
adminnn <geojesi1@mail.ru>
2025-05-15 17:16:55 +0400

    add new fanc and fix

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-import webbrowser
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class SimpleHandler(BaseHTTPRequestHandler):
-    def do_GET(self) -> None:
-        try:
-            with open("contacts.html", "r", encoding="utf-8") as file:
-                html_content = file.read()
+    def do_GET(self):
+        with open("contacts.html", "r", encoding="utf-8") as file:
+            html = file.read()
 
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=utf-8")
-            self.end_headers()
-            self.wfile.write(html_content.encode("utf-8"))
-
-        except FileNotFoundError:
-            self.send_response(404)
-            self.send_header("Content-type", "text/html; charset=utf-8")
-            self.end_headers()
-            self.wfile.write("Файл contacts.html не найден.".encode("utf-8"))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(html.encode("utf-8"))
 
 
 if __name__ == "__main__":
     server_address = ("", 8000)
     httpd = HTTPServer(server_address, SimpleHandler)
     print("Сервер запущен на http://localhost:8000")
-    webbrowser.open("http://localhost:8000")
-
     httpd.serve_forever()
